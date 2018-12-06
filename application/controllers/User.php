ef5cb66eebf9489bb26455ce5df52492ef537297
irryasa <ahmadillyasa2056@gmail.com>
2018-12-06 22:25:54 +0700

    post user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -21,6 +21,24 @@ class User extends REST_Controller {
         
         return $this->response(array('status' => 200,'result'=>$query->result()));  
     }
+
+    function index_post(){
+        $data = array(
+                'nim' => $this->post('nim'),
+                'nama' => $this->post('nama'),
+                'email' => $this->post('email'),
+                'password' => $this->post('password'),
+                'img' => $this->post('img'),
+                'level' => $this->post('level')
+            );
+
+        $insert = $this->db->insert('users', $data);
+        if ($insert){
+            $this->response($data, 200);
+        }else{
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
     
 
 } ?>
